f69610a14974621c93501e3b8ce60936bfe21865
Israel <s.israelcardoso@gmail.com>
2021-12-30 12:54:25 -0300

    Limpei o codigo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package course;
-
-/**
- *
- * @author Israel
- */
 public class Program {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       
-        System.out.println("BOm dia");
+        System.out.println("Bom dia");
     }
     
 }
